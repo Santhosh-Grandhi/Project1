56d6c6c2cac82ce7991fc2568cc0558e8d7a66b0
SanthoshGrandhi <santhoshgrandhi89@gmail.com>
2024-07-18 20:32:22 +0530

    MySQL Commit

--- MySql.sh
+++ MySql.sh
@@ -1,14 +1,15 @@
 source CommonFile.sh
+rm -f /tmp/expense.log
 
 Heading "Install MySql Server"
-dnf install mysql-server -y
+dnf install mysql-server -y &>> /tmp/expense.log
 STAT $?
 
 Heading "Enable and Start MYSQL"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>> /tmp/expense.log
+systemctl start mysqld &>> /tmp/expense.log
 STAT $?
 
 Heading "Set MySql Password"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>> /tmp/expense.log
 STAT $?
