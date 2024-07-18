802cd2bff0f1a7424402819a617c274a603cacf7
SanthoshGrandhi <santhoshgrandhi89@gmail.com>
2024-07-18 19:59:29 +0530

    front end

--- Frontend.sh
+++ Frontend.sh
@@ -1,31 +1,32 @@
 source CommonFile.sh
+rm -f /tmp/expense.log
 
 Heading "Installing Ngnix"
-dnf install nginx -y
+dnf install nginx -y &>> /tmp/expense.log
 STAT $?
 
 Heading "Enable and start Ngnix"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> /tmp/expense.log
+systemctl start nginx &>> /tmp/expense.log
 STAT $?
 
 Heaing "Copy Expense file"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>> /tmp/expense.log
 STAT $?
 
 Heading "Remove default content"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>> /tmp/expense.log
 STAT $?
 
 Heading "Download Frontend content"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-cd /usr/share/nginx/html
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>> /tmp/expense.log
+cd /usr/share/nginx/html &>> /tmp/expense.log
 STAT $?
 
 Heading "Unzip content"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /tmp/expense.log
 STAT $?
 
 Heading "Restart Ngnix"
-systemctl restart nginx
+systemctl restart nginx &>> /tmp/expense.log
 STAT $?
