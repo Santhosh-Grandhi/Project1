faed9b41d5d9f092ab630f4439ab6b3892c3f568
SanthoshGrandhi <santhoshgrandhi89@gmail.com>
2024-07-18 22:46:59 +0530

    MySQL Commit

--- MySql.sh
+++ MySql.sh
@@ -1,6 +1,11 @@
 source CommonFile.sh
 rm -f /tmp/expense.log
 
+if [ -z "$1" ]; then
+  echo "Input is missing"
+  exit 1
+fi
+
 Heading "Install MySql Server"
 dnf install mysql-server -y &>> /tmp/expense.log
 STAT $?
@@ -11,5 +16,5 @@ systemctl start mysqld &>> /tmp/expense.log
 STAT $?
 
 Heading "Set MySql Password"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>> /tmp/expense.log
+mysql_secure_installation --set-root-pass $1 &>> /tmp/expense.log
 STAT $?
